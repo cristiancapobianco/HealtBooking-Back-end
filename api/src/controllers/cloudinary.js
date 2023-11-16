cde65f9087440a507f2bf5dc83e4ac9526497028
cristiancapobianco <127978387+cristiancapobianco@users.noreply.github.com>
2023-11-16 20:52:45 -0300

    d

---
+++ api/src/controllers/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dufgoaqph',
+    api_key: '258896447691478',
+    api_secret: 'xSYoQZ5izWBTv1gh9j9nl-TkdPk',
+});
+
+module.exports = cloudinary;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,15 +1,16 @@
 const { Router } = require('express');
 const router = Router();
+const cloudinary = require('../controllers/cloudinary')
 
-router.get("/", async (req, res) => {
-    res.send("hola")
-})
 
+
+//faltaria moduralizar las enrutaros
 router.post("/crear", async (req, res) => {
     try {
         const newDoc = req.body
-        const { name, lastName, tuition, email, } = newDoc
+        const { name, lastName, tuition, email, image } = newDoc
 
+        //nombre de los modelos a definir
         const existingDoc = await ModelDoc.findOne({
             where: {
                 tuition
@@ -20,7 +21,16 @@ router.post("/crear", async (req, res) => {
             res.send({ message: "La matricula ya esta registrada" })
         } else {
             try {
-                const doc = await ModelDoc.create({ name, lastName, tuition, email })
+                cloudinary.uploader.upload(image, (error, result) => {
+                    if (error) {
+                        console.error('Error al subir la imagen a Cloudinary:', error);
+                        res.status(500).send('Error al subir la imagen');
+                    } else {
+                        console.log('Imagen subida con éxito:', result);
+                        const imageUrl = result.secure_url;
+                    }
+                });
+                const doc = await ModelDoc.create({ name, lastName, tuition, email, imageUrl })
                 /* 
                 falta verificar como mandarian las especialidades y manjarlas
                 falta hacer las relaciones de las especialidades con los doctores
