18d68938297dd24ce11355950080c2d18b9821ff
cristiancapobianco <127978387+cristiancapobianco@users.noreply.github.com>
2023-11-16 01:38:03 -0300

    post

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,40 @@
 const { Router } = require('express');
 const router = Router();
 
-
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     res.send("hola")
+})
+
+router.post("/crear", async (req, res) => {
+    try {
+        const newDoc = req.body
+        const { name, lastName, tuition, email, } = newDoc
+
+        const existingDoc = await ModelDoc.findOne({
+            where: {
+                tuition
+            }
+        })
+
+        if (existingDoc) {
+            res.send({ message: "La matricula ya esta registrada" })
+        } else {
+            try {
+                const doc = await ModelDoc.create({ name, lastName, tuition, email })
+                /* 
+                falta verificar como mandarian las especialidades y manjarlas
+                falta hacer las relaciones de las especialidades con los doctores
+                */
+
+                res.send("Doctor creado", doc)
+            } catch (error) {
+                res.send("Error al crear doctor", error.message)
+            }
+        }
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 })
 
 module.exports = router
