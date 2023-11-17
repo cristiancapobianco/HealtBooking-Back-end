299558b2a6664e61fe0418a0846d69d08b44f389
1W3111990ChaparroSantiago <69601377+1W3111990ChaparroSantiago@users.noreply.github.com>
2023-11-17 20:07:22 -0300

    base de datos

---
+++ api/src/models/Medico.js
@@ -0,0 +1,28 @@
+const {DataTypes} = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define("Medico",{
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        nombre:{
+           type: DataTypes.STRING,
+           allowNull: false, 
+        },
+        apellido:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        mail:{
+            type: DataTypes.STRING,
+            unique: true,
+            validate:{
+                isEmail:{
+                    msg: "Ingrese una dirección de correo electrónico válida",
+                },
+            },
+
+        },
+    })
+}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,4 +6,5 @@ router.get("/", (req, res) => {
     res.send("hola")
 })
 
+
 module.exports = router
