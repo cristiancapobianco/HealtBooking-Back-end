ea1b1c71cc0074eab7f1b70936d9799cef8a606c
cristiancapobianco <127978387+cristiancapobianco@users.noreply.github.com>
2023-11-18 01:51:12 -0300

    post terminado

--- api/src/controllers/postDoctor.js
+++ api/src/controllers/postDoctor.js
@@ -6,9 +6,8 @@ const { Doctors, Specialty, Sure } = require('../db')
 const postDoctor = async (req, res) => {
     try {
         const newDoc = req.body
-        const { name, lastName, tuition, email, image } = newDoc
+        const { name, id, email, profilePicture, sure, specialty } = newDoc
 
-        //nombre de los modelos a definir
         const existingDoc = await Doctors.findOne({
             where: {
                 tuition
@@ -19,11 +18,31 @@ const postDoctor = async (req, res) => {
             res.send({ message: "La matricula ya esta registrada" })
         } else {
             try {
-                const doc = await Doctors.create({ name, lastName, tuition, email, imageUrl })
-                /* 
-                falta verificar como mandarian las especialidades y manjarlas
-                falta hacer las relaciones de las especialidades con los doctores
-                */
+                const doc = await Doctors.create({ name, id, email, profilePicture })
+
+                for (const sureName of sure) {
+                    const existingSure = await Sure.findOne({
+                        where: { name: sureName }
+                    });
+
+                    if (existingSure) {
+                        await Doctors.addSure(existingSure);
+                    } else {
+                        console.log(`Sure '${sureName}' no encontrado.`);
+                    }
+                }
+
+                if (specialty) {
+                    const existingSpecialty = await Specialty.findOne({
+                        where: { name: specialty }
+                    });
+
+                    if (existingSpecialty) {
+                        await Doctors.addSpecialty(existingSpecialty);
+                    } else {
+                        res.send(`Especialidad '${specialty}' no encontrada.`);
+                    }
+                }
 
                 res.send("Doctor creado", doc)
             } catch (error) {
