6ecc7e9f702fdf868e32b9aeee0a6c060b7b6b31
cristiancapobianco <127978387+cristiancapobianco@users.noreply.github.com>
2023-11-19 00:10:22 -0300

    done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const { getAllDoctors } = require("../controllers/getAllDoctors")
 const { postDoctor } = require("../controllers/postDoctor")
-const { detailDoctor } = require("../controllers/detailDoctors")
+const { detailDoctor } = require("../controllers/detailDoctors");
+
 
 const router = Router();
 
 router.get('/doctors', getAllDoctors);
 router.get('/doctors/:idDoc', detailDoctor);
 router.post('/doctor', postDoctor);
-
 
 module.exports = router
