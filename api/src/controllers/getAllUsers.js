7bce20d7b83f528fbbe700df003e7c736c6972af
1W3111990ChaparroSantiago <69601377+1W3111990ChaparroSantiago@users.noreply.github.com>
2023-11-24 19:50:03 -0300

    se hizo de nuevo el controller de getAllUsers

--- api/src/controllers/getAllUsers.js
+++ api/src/controllers/getAllUsers.js
@@ -3,37 +3,22 @@ const { Doctor, Specialty, Sure, DoctorSure, Patient, DoctorPatient } = require(
 const getAllUsers = async (req, res) => {
 
     try {
-        const data = await Doctor.findAll({
-            attributes: ['id', 'name', 'profilePicture', 'state'], 
-            include: [
-                {
-                    model: Specialty,
-                    attributes: ['id']
-                },
-                {
-                    model: Sure,
-                    attributes: ['id'],
-                    through: {
-                        model: DoctorSure,
-                        attributes: []
-                    },
-                },
-                {
-                    model: Patient,
-                    attributes: ['id', 'name', 'state'],
-                    through: {
-                        model: DoctorPatient, 
-                        attributes: []
-                    },
-                    where: {
-                        state: ['active', 'inactive']
-                    }
-                }
-            ]
+        const doctors = await Doctor.findAll({
+            attributes: ['id', 'name', 'profilePicture', 'state'],
         });
 
-        return res.status(200).json(data);
-        
+        const patients = await Patient.findAll({
+            attributes: ['id', 'name', 'state'],
+            where: {
+                state: ['active', 'inactive'],
+            },
+        });
+
+        const allUsers = [...doctors, ...patients];
+
+        console.log(allUsers);
+
+        return res.status(200).json(allUsers);
     } catch (error) {
         return res.status(500).json({ message: 'Error al encontrar médicos y pacientes', error: error.message });
     }
