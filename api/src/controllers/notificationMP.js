9871060fc6359a1a3ac1c65643886883f7dfede1
Santiago Paz <santipaz19.02@gmail.com>
2023-12-02 02:09:28 -0300

    cambios

--- api/src/controllers/notificationMP.js
+++ api/src/controllers/notificationMP.js
@@ -1,11 +1,11 @@
 const { Appointment } = require("../db");
 
 const notifyPay = async (req, res) => {
-    const { external_reference } = req.query;
-    console.log(external_reference)
+    const data = req.body;
+    console.log(data)
     try {
         // if (id === "123456789") {
-        //     return res.status(200).json({ message: 'Estado de la cita actualizado con éxito', id });
+        return res.status(200).json({ message: 'Estado de la cita actualizado con éxito', data });
         // }
         const appointment = await Appointment.findOne({
             where: { id: external_reference }
