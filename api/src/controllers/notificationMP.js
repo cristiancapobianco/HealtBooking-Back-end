bf2128cfc1f29a5980f82ec845fb394677b78dad
Santiago Paz <santipaz19.02@gmail.com>
2023-12-02 01:51:28 -0300

    pasarela funcionando

--- api/src/controllers/notificationMP.js
+++ api/src/controllers/notificationMP.js
@@ -1,23 +1,22 @@
 const { Appointment } = require("../db");
 
 const notifyPay = async (req, res) => {
-    const data = req.body;
-    const { id } = data.data
+    const { external_reference } = req.query;
+
     try {
-        if (id === "123456789") {
-            return res.status(200).json({ message: 'Estado de la cita actualizado con éxito', id });
-        }
+        // if (id === "123456789") {
+        //     return res.status(200).json({ message: 'Estado de la cita actualizado con éxito', id });
+        // }
         const appointment = await Appointment.findOne({
-            where: { id: data.id }
+            where: { id: external_reference }
         });
         if (!appointment) {
             return res.status(404).json({ error: 'Cita no encontrada' });
         }
 
         await appointment.update({ status: 'paid' });
-        console.log(data)
 
-        return res.status(200).json({ message: 'Estado de la cita actualizado con éxito', data });
+        return res.status(200).json({ message: 'Estado de la cita actualizado con éxito', external_reference });
     } catch (error) {
         console.error('Error al procesar la notificación de MercadoPago:', error);
         return res.status(500).json({ error: 'Error interno del servidor' });
